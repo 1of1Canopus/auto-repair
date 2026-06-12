e4d1c2ab45fb35b07e3dee74a5ccf90d35f7a8e5
Canopus <souhaile.safouani@gmail.com>
2026-06-12 11:51:44 +0200

    (feat) AR-01 : scaffold spring boot app with core deps

---
+++ src/test/java/com/housedevinci/autorepair/AutoRepairApplicationTests.java
@@ -0,0 +1,12 @@
+package com.housedevinci.autorepair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRepairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
